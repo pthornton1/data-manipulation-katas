3fffac2dd605fd93164e0091bce980d5931041f5
Philip Thornton <philipthornton198@gmail.com>
2025-04-16 10:54:37 +0100

    complete createRef

--- __tests__/test.createRef.js
+++ __tests__/test.createRef.js
@@ -1,1 +1,81 @@
-import createRef from "../createRef";
+const { createRef } = require("../createRef");
+
+describe("createRef", () => {
+  test("returns an empty object when passed an empty array", () => {
+    // Arrange
+    const testEmployees = [];
+    const inputKey = "name";
+    const inputValue = "id";
+    // Act
+    const outputObj = createRef(testEmployees, inputKey, inputValue);
+    // Assert
+    expect(outputObj).toEqual({});
+  });
+  test("returns object with correct key value pair when passed an array of length one", () => {
+    // Arrange
+    const testEmployees = [
+      { name: "Rose", id: "dS8rJns", secretFear: "spiders" },
+    ];
+    const inputKey = "name";
+    const inputValue = "id";
+    // Act
+    const outputObj = createRef(testEmployees, inputKey, inputValue);
+    // Assert
+    expect(outputObj).toEqual({ Rose: "dS8rJns" });
+  });
+  test("returns object with correct key value pairs when passed an array of length greater than one", () => {
+    // Arrange
+    const testEmployees = [
+      { name: "Rose", id: "dS8rJns", secretFear: "spiders" },
+      { name: "Simon", id: "Pk34ABs", secretFear: "mice" },
+      { name: "Jim", id: "lk1ff8s", secretFear: "bears" },
+      { name: "David", id: "og8r0nV", secretFear: "Rose" },
+    ];
+    const inputKey = "name";
+    const inputValue = "id";
+    // Act
+    const outputObj = createRef(testEmployees, inputKey, inputValue);
+    // Assert
+    expect(outputObj).toEqual({
+      Rose: "dS8rJns",
+      Simon: "Pk34ABs",
+      Jim: "lk1ff8s",
+      David: "og8r0nV",
+    });
+  });
+  test("ignores objects in passed array if they don't contain the key passed", () => {
+    // Arrange
+    const testEmployees = [
+      { name: "Rose", id: "dS8rJns", secretFear: "spiders" },
+      { name: "Simon", id: "Pk34ABs", secretFear: "mice" },
+      { name: "Jim", id: "lk1ff8s", secretFear: "bears" },
+      { surname: "David", id: "og8r0nV", secretFear: "Rose" },
+    ];
+    const inputKey = "surname";
+    const inputValue = "id";
+    // Act
+    const outputObj = createRef(testEmployees, inputKey, inputValue);
+    // Assert
+    expect(outputObj).toEqual({ David: "og8r0nV" });
+  });
+  test("sets values to undefined if input employees don't have the value passed", () => {
+    // Arrange
+    const testEmployees = [
+      { name: "Rose", id: "dS8rJns", secretFear: "spiders" },
+      { name: "Simon", id: "Pk34ABs", secretFear: "mice" },
+      { name: "Jim", id: "lk1ff8s", secretFear: "bears" },
+      { name: "David", secretFear: "Rose" },
+    ];
+    const inputKey = "name";
+    const inputValue = "id";
+    // Act
+    const outputObj = createRef(testEmployees, inputKey, inputValue);
+    // Assert
+    expect(outputObj).toEqual({
+      Rose: "dS8rJns",
+      Simon: "Pk34ABs",
+      Jim: "lk1ff8s",
+      David: undefined,
+    });
+  });
+});
--- createRef.js
+++ createRef.js
@@ -1,1 +1,12 @@
-export default function createRef() {}
+function createRef(employees, key, value) {
+  const referenceObj = {};
+  for (let i = 0; i < employees.length; i++) {
+    const currentEmployee = employees[i];
+    if (currentEmployee.hasOwnProperty(key)) {
+      referenceObj[currentEmployee[key]] = currentEmployee[value];
+    }
+  }
+  return referenceObj;
+}
+
+module.exports = { createRef };
